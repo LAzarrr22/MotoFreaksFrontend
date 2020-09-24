e4919b5533017c9aacb580163b7b02e34cc6fdff
LAzarrr22 <maciek.janik24@gmail.com>
2020-09-24 23:03:54 +0200

    add home component with modules

---
+++ MotoFreaksFrontend/src/app/home/home-routing.module.ts
@@ -0,0 +1,18 @@
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes),
+  ],
+})
+export class HomeRoutingModule {
+}
