00dc40f995f06c515816a0dc0bc7f7fb032a1b21
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-01 20:42:01 +0200

    add menu side

---
+++ MotoFreaksFrontend/src/app/modules/menu/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,49 @@
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AppPath} from "../../../../shared/enums/app-path.enum";
+import {MenuService} from "../../logic/services/menu.service";
+import {Store} from "@ngrx/store";
+import {AuthenticationState, isUserLoggedIn} from "../../../authentication/logic/store";
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.scss']
+})
+export class MenuBarComponent implements OnInit {
+
+  activeRoute: string = '';
+  isLogged: boolean;
+
+  constructor(private router: Router, private changeDetector: ChangeDetectorRef, private menuService: MenuService, private authStore: Store<AuthenticationState>) {
+    this.authStore.select(isUserLoggedIn).subscribe(isLoggedState => this.isLogged = isLoggedState)
+  }
+
+  ngOnInit() {
+    this.menuService.activeRoute.subscribe(activeElement => {
+      this.activeRoute = activeElement;
+      this.changeDetector.detectChanges();
+    })
+  }
+
+
+  goToHome() {
+    this.router.navigate([AppPath.HOME_PATH]);
+  }
+
+  logOut() {
+    this.router.navigate([AppPath.AUTH_LOGOUT_PATH])
+  }
+
+  goToRegister() {
+    this.router.navigate([AppPath.AUTH_REGISTER_PATH])
+  }
+
+  goToLogin() {
+    this.router.navigate([AppPath.AUTH_LOGIN_PATH])
+  }
+
+  goToMeProfile() {
+
+  }
+}
---
+++ MotoFreaksFrontend/src/app/shared/enums/active-route.enum.ts
@@ -0,0 +1,7 @@
+export enum ActiveRoute {
+
+  LOGIN = '[Active] LOGIN',
+  REGISTER = '[Active] REGISTER',
+  HOME = '[Active] HOME',
+  MY_PROFILE = '[Active] MY_PROFILE',
+}
