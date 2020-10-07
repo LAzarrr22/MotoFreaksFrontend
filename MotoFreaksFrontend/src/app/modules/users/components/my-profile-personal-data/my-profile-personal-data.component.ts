9d21c43e3cccef3723656c809881a55f89ac9ad0
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-07 20:28:54 +0200

    implements components for profile

---
+++ MotoFreaksFrontend/src/app/modules/users/components/my-profile-personal-data/my-profile-personal-data.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {MyProfileModel} from "../../logic/dto/response/my-profile.model";
+
+@Component({
+  selector: 'app-my-profile-personal-data',
+  templateUrl: './my-profile-personal-data.component.html',
+  styleUrls: ['./my-profile-personal-data.component.scss']
+})
+export class MyProfilePersonalDataComponent implements OnInit {
+  @Input()
+  profile: MyProfileModel;
+  @Input()
+  isLoading: boolean;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- MotoFreaksFrontend/src/app/modules/users/pages/profile-me/profile-me.component.ts
+++ MotoFreaksFrontend/src/app/modules/users/pages/profile-me/profile-me.component.ts
@@ -2,8 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActiveRoute} from "../../../../shared/enums/active-route.enum";
 import {MenuService} from "../../../menu/logic/services/menu.service";
 import {Store} from "@ngrx/store";
-import {getMyProfile, MyProfileState} from "../../logic/reducers/my-profile.reducers";
-import {GetMyProfile} from "../../logic/action/my-profile.action";
+import {getLoading, getMyProfile, MyProfileState} from "../../logic/reducers/my-profile.reducers";
 import {Observable} from "rxjs";
 import {MyProfileModel} from "../../logic/dto/response/my-profile.model";
 
@@ -15,10 +14,11 @@ import {MyProfileModel} from "../../logic/dto/response/my-profile.model";
 export class ProfileMeComponent implements OnInit {
 
   profile: Observable<MyProfileModel>;
+  isLoading: Observable<boolean>;
 
   constructor(private menuService: MenuService, private store: Store<MyProfileState>) {
-    this.store.dispatch(new GetMyProfile());
     this.profile = this.store.select(getMyProfile);
+    this.isLoading = this.store.select(getLoading);
 
   }
 
