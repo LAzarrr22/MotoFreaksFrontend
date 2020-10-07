ac41ea2bb8069d2588b8eda0a2f72f7deddf5b75
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-07 20:22:12 +0200

    API enabled cors policy

--- MotoFreaksBackend/src/main/java/com/MJ/MotoFreaksBackend/MotoFreaksBackend/resource/controller/UserController.java
+++ MotoFreaksBackend/src/main/java/com/MJ/MotoFreaksBackend/MotoFreaksBackend/resource/controller/UserController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/user")
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserService userService;
