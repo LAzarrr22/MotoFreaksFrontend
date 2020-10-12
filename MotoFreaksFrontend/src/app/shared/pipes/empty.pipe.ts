3beb323295c358ce40b9e2b34d091176eaebec9a
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-12 16:14:11 +0200

    add pipe condition when value is empty or null

--- MotoFreaksFrontend/src/app/shared/pipes/empty.pipe.ts
+++ MotoFreaksFrontend/src/app/shared/pipes/empty.pipe.ts
@@ -3,6 +3,9 @@ import {Pipe, PipeTransform} from "@angular/core";
 @Pipe({name: 'empty'})
 export class EmptyPipe implements PipeTransform {
   transform(value: any): any {
-    return value ?? '-'
+    if (value == null || value == '') {
+      return '-'
+    }
+    return value
   }
 }
