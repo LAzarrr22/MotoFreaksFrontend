09f2d4413e22e13ebfbb374d8d1a48466dbc7cec
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-12 10:14:18 +0200

    add merge contact template

--- MotoFreaksFrontend/src/app/modules/users/components/my-profile-all-data/section-contact/section-contact.component.ts
+++ MotoFreaksFrontend/src/app/modules/users/components/my-profile-all-data/section-contact/section-contact.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ContactModel} from "../../../logic/dto/models/contact.model";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-section-contact',
@@ -11,14 +12,29 @@ export class SectionContactComponent implements OnInit {
   @Input()
   contact: ContactModel;
   isEditable: boolean = false;
+  formMerge: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.formMerge = this.formBuilder.group({
+      email: new FormControl(this.contact.email, [Validators.required]),
+      phone: new FormControl(this.contact.phone),
+      skype: new FormControl(this.contact.skype),
+      facebook: new FormControl(this.contact.facebook),
+      instagram: new FormControl(this.contact.instagram),
+      twitter: new FormControl(this.contact.twitter),
+    });
   }
 
   editData() {
     this.isEditable = !this.isEditable;
+  }
+
+  mergeContact() {
+    if (this.formMerge.valid) {
+      console.dir(this.formMerge)
+    }
   }
 }
