ae69bf504ce2b4a6b4985df3cf58ced0a13c956a
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-12 09:53:02 +0200

    add state field to show, add merge address template

--- MotoFreaksFrontend/src/app/modules/users/components/my-profile-all-data/section-address/section-address.component.ts
+++ MotoFreaksFrontend/src/app/modules/users/components/my-profile-all-data/section-address/section-address.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {AddressModel} from "../../../logic/dto/models/address.model";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-section-address',
@@ -10,14 +11,27 @@ export class SectionAddressComponent implements OnInit {
   @Input()
   address: AddressModel
   isEditable: boolean = false;
+  formMerge: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.formMerge = this.formBuilder.group({
+      country: new FormControl(this.address.country, [Validators.required]),
+      state: new FormControl(this.address.state, [Validators.required]),
+      city: new FormControl(this.address.city, [Validators.required]),
+      street: new FormControl(this.address.street),
+    });
   }
 
   editData() {
     this.isEditable = !this.isEditable;
+  }
+
+  mergeAddress() {
+    if (this.formMerge.valid) {
+      console.dir(this.formMerge)
+    }
   }
 }
--- MotoFreaksFrontend/src/app/modules/users/logic/dto/models/address.model.ts
+++ MotoFreaksFrontend/src/app/modules/users/logic/dto/models/address.model.ts
@@ -2,11 +2,13 @@ export class AddressModel {
   country: string;
   city: string;
   street: string;
+  state: string;
 
 
-  constructor(country: string, city: string, street: string) {
+  constructor(country: string, city: string, street: string, state: string) {
     this.country = country;
     this.city = city;
     this.street = street;
+    this.state = state;
   }
 }
