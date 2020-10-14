c017bf423ff63be7c2ffc7a1e61bcc346fde30bc
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-14 09:16:55 +0200

    handling edit/new car template at profile page, enable disable, generate from model

--- MotoFreaksFrontend/src/app/modules/users/components/my-profile-all-data/section-cars/add-edit-car/add-edit-car.component.ts
+++ MotoFreaksFrontend/src/app/modules/users/components/my-profile-all-data/section-cars/add-edit-car/add-edit-car.component.ts
@@ -12,32 +12,50 @@ export class AddEditCarComponent implements OnInit {
   @Input()
   car: CarModel;
   formMerge: FormGroup;
-
+  @Input()
+  isCarEdit: boolean = false;
+  @Input()
+  isCarAdd: boolean = false;
   @Output()
-  closeEditing = new EventEmitter();
+  closeTemplate = new EventEmitter();
 
 
   constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
-    this.formMerge = this.formBuilder.group({
-      name: new FormControl(this.car.name, [Validators.required]),
-      registration: new FormControl(this.car.registration),
-      company: new FormControl(this.car.company),
-      model: new FormControl(this.car.model),
-      generation: new FormControl(this.car.generation),
-      year: new FormControl(this.car.year),
-      color: new FormControl(this.car.color),
-      engine: new FormControl(this.car.engine),
-      horsepower: new FormControl(this.car.horsepower),
-      torque: new FormControl(this.car.torque),
-    });
+    if (this.isCarEdit && this.car) {
+      this.formMerge = this.formBuilder.group({
+        name: new FormControl(this.car.name, [Validators.required]),
+        registration: new FormControl(this.car.registration),
+        company: new FormControl(this.car.company),
+        model: new FormControl(this.car.model),
+        generation: new FormControl(this.car.generation),
+        year: new FormControl(this.car.year),
+        color: new FormControl(this.car.color),
+        engine: new FormControl(this.car.engine),
+        horsepower: new FormControl(this.car.horsepower),
+        torque: new FormControl(this.car.torque),
+      });
+    } else {
+      this.formMerge = this.formBuilder.group({
+        name: new FormControl('', [Validators.required]),
+        registration: new FormControl(''),
+        company: new FormControl(''),
+        model: new FormControl(''),
+        generation: new FormControl(''),
+        year: new FormControl(''),
+        color: new FormControl(''),
+        engine: new FormControl(''),
+        horsepower: new FormControl(''),
+        torque: new FormControl(''),
+      });
+    }
   }
 
   mergeCar() {
     console.dir(this.formMerge)
-    this.closeEditing.emit();
+    this.closeTemplate.emit();
   }
 }
 
