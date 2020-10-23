f9f6391adb1cdd41ac1e5d324475e458e1ef9cfc
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-23 14:17:39 +0200

    Angular add action to get post by type or id

--- MotoFreaksFrontend/src/app/modules/posts/logic/action/posts.action.ts
+++ MotoFreaksFrontend/src/app/modules/posts/logic/action/posts.action.ts
@@ -1,6 +1,7 @@
 import {Action} from "@ngrx/store";
 import {NewPostModel} from "../dto/request/new-post.model";
 import {PostModel} from "../dto/model/post.model";
+import {PostType} from "../enums/post-type.enum";
 
 
 export const GET_ALL_POST = '[Posts] GET_ALL_POST'
@@ -10,6 +11,14 @@ export const GET_ALL_POST_FAIL = '[Posts] GET_ALL_POST_FAIL'
 export const GET_ALL_MY_POST = '[Posts] GET_ALL_MY_POST'
 export const GET_ALL_MY_POST_SUCCESS = '[Posts] GET_ALL_MY_POST_SUCCESS'
 export const GET_ALL_MY_POST_FAIL = '[Posts] GET_ALL_MY_POST_FAIL'
+
+export const GET_ALL_POST_BY_ID = '[Posts] GET_ALL_POST_BY_ID'
+export const GET_ALL_POST_BY_ID_SUCCESS = '[Posts] GET_ALL_POST_BY_ID_SUCCESS'
+export const GET_ALL_POST_BY_ID_FAIL = '[Posts] GET_ALL_POST_BY_ID_FAIL'
+
+export const GET_ALL_POST_BY_TYPE = '[Posts] GET_ALL_POST_BY_ID'
+export const GET_ALL_POST_BY_TYPE_SUCCESS = '[Posts] GET_ALL_POST_BY_ID_SUCCESS'
+export const GET_ALL_POST_BY_TYPE_FAIL = '[Posts] GET_ALL_POST_BY_ID_FAIL'
 
 export const DELETE_POST = '[Posts] DELETE_POST'
 export const DELETE_POST_SUCCESS = '[Posts] DELETE_POST_SUCCESS'
@@ -62,6 +71,49 @@ export class GetAllMyPostsFail implements Action {
   }
 }
 
+export class GetAllPostById implements Action {
+  readonly type = GET_ALL_POST_BY_ID;
+
+  constructor(public id: string) {
+  }
+}
+
+export class GetAllPostByIdSuccess implements Action {
+  readonly type = GET_ALL_POST_BY_ID_SUCCESS;
+
+  constructor(public payload: PostModel[]) {
+  }
+}
+
+export class GetAllPostByIdFail implements Action {
+  readonly type = GET_ALL_POST_BY_ID_FAIL;
+
+  constructor(public payload: string) {
+  }
+}
+
+
+export class GetAllPostByType implements Action {
+  readonly type = GET_ALL_POST_BY_TYPE;
+
+  constructor(public typePost: PostType) {
+  }
+}
+
+export class GetAllPostByTypeSuccess implements Action {
+  readonly type = GET_ALL_POST_BY_TYPE_SUCCESS;
+
+  constructor(public payload: PostModel[]) {
+  }
+}
+
+export class GetAllPostByTypeFail implements Action {
+  readonly type = GET_ALL_POST_BY_TYPE_FAIL;
+
+  constructor(public payload: string) {
+  }
+}
+
 export class DeletePost implements Action {
   readonly type = DELETE_POST;
 
@@ -108,9 +160,15 @@ export type Actions =
   | GetAllMyPosts
   | GetAllMyPostsSuccess
   | GetAllMyPostsFail
+  | GetAllPostById
+  | GetAllPostByIdSuccess
+  | GetAllPostByIdFail
   | GetAllPosts
   | GetAllPostsSuccess
   | GetAllPostsFail
+  | GetAllPostByType
+  | GetAllPostByTypeSuccess
+  | GetAllPostByTypeFail
   | DeletePost
   | DeletePostSuccess
   | DeletePostFail
