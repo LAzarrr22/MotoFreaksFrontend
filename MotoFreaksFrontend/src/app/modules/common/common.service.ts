bee18039533fbacf660ba466eea5459f7c747d5a
LAzarrr22 <maciek.janik24@gmail.com>
2020-10-25 18:12:21 +0100

    logout user when api not response

--- MotoFreaksFrontend/src/app/modules/common/common.service.ts
+++ MotoFreaksFrontend/src/app/modules/common/common.service.ts
@@ -2,16 +2,20 @@ import {Router} from "@angular/router";
 import {Injectable} from "@angular/core";
 import {AppPath} from "../../shared/enums/app-path.enum";
 import {MenuService} from "../menu/logic/services/menu.service";
+import {Store} from "@ngrx/store";
+import {AuthenticationState} from "../authentication/logic/store";
+import {UserLogout} from "../authentication/logic/actions/authentication.actions";
 
 @Injectable()
 export class CommonComponentsService {
 
-  constructor(private router: Router, private menuService: MenuService) {
+  constructor(private router: Router, private menuService: MenuService, private readonly store: Store<AuthenticationState>) {
 
   }
 
   error(error: any) {
     if (error.status === 0) {
+      this.store.dispatch(new UserLogout());
       this.router.navigate([AppPath.SERVICE_NOT_AVAILABLE])
       this.menuService.enabled.next('false')
     }
