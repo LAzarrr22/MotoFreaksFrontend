29721809df7d56c6dabb062db2c1c829ef3439b5
LAzarrr22 <maciek.janik24@gmail.com>
2020-11-02 22:42:02 +0100

    add header policy

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.static('./dist/MotoFreaksFrontend'));
 
+app.use(function (req,res,next) {
+  res.header("Access-Control-Allow-Origin", "*");
+next()
+})
+
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/MotoFreaksFrontend/'}),
 );
