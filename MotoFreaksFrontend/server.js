fd07f825fcc66e695402e64f39533ac6ffa5ff55
LAzarrr22 <maciek.janik24@gmail.com>
2020-11-02 17:45:45 +0100

    add node server

---
+++ MotoFreaksFrontend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/MotoFreaksFrontend'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/MotoFreaksFrontend/'}),
+);
+
+app.listen(process.env.PORT || 8080);
