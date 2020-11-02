3e4e158bec118f701f79a62078c53f2446f0b028
LAzarrr22 <maciek.janik24@gmail.com>
2020-11-02 21:03:58 +0100

    change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://moto-freak-backend.herokuapp.com/',
   jwtExpirationTime: 3600,
   localstorageSessionKey: 'sessionMTFR'
 };
