7969275527e036ae0ac924b039785e79ddbda72a
Maciej Janik <48676314+LAzarrr22@users.noreply.github.com>
2020-11-03 16:49:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const app = express();
 
 app.use(express.static('./dist/MotoFreaksFrontend'));
 
-app.use(cors())
-
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/MotoFreaksFrontend/'}),
 );
