ba092932b589cf69a3f53ad596b4711ae46b84ae
LAzarrr22 <maciek.janik24@gmail.com>
2020-11-03 16:30:08 +0100

    add cors to server

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const app = express();
 
 app.use(express.static('./dist/MotoFreaksFrontend'));
 
-app.use(function (req,res,next) {
-  res.header("Access-Control-Allow-Origin", "*");
-next()
-})
+app.use(cors())
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/MotoFreaksFrontend/'}),
