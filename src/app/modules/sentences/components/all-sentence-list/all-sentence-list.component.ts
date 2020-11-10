ffe046a3b6117df71b511a0376dbc6c872eb52f4
LAzarrr22 <maciek.janik24@gmail.com>
2020-11-10 20:55:18 +0100

    implements table for shows translations

--- src/app/modules/sentences/components/all-sentence-list/all-sentence-list.component.ts
+++ src/app/modules/sentences/components/all-sentence-list/all-sentence-list.component.ts
@@ -1,20 +1,52 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {SentenceModel} from "../../logic/model/sentence.model";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-all-sentence-list',
   templateUrl: './all-sentence-list.component.html',
   styleUrls: ['./all-sentence-list.component.scss']
 })
-export class AllSentenceListComponent implements OnInit {
+export class AllSentenceListComponent implements OnInit, AfterViewInit {
+
+
+
+  columnNames = {
+    name: 'Sentence',
+    translation: 'Translation',
+  };
+  displayedColumns = Object.keys(this.columnNames);
+
+  dataSource: MatTableDataSource<SentenceModel>;
+
+  resultsLength = 0;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   @Input()
   sentencesList:SentenceModel[]
 
+  constructor() {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.sentencesList);
+
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
