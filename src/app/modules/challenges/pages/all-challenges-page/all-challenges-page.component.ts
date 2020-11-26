a39c3c6f32c2998b6c4bf0a3bb3fa579471d7fde
LAzarrr22 <maciek.janik24@gmail.com>
2020-11-26 23:28:09 +0100

    start handling filtering

--- src/app/modules/challenges/pages/all-challenges-page/all-challenges-page.component.ts
+++ src/app/modules/challenges/pages/all-challenges-page/all-challenges-page.component.ts
@@ -6,12 +6,13 @@ import {Observable} from "rxjs";
 import {ChallengeDtoModel} from "../../logic/dto/response/challenge-dto.model";
 import {ProfileService} from "../../../profiles/logic/services/profile.service";
 import {Actions, ofType} from "@ngrx/effects";
-import {GET_ALL_CHALLENGES_BY_CAR_FAIL, GetAllChallengesByCarFail} from "../../logic/actions/challenges.actions";
 import {map} from "rxjs/operators";
 import {Router} from "@angular/router";
 import {AppPath} from "../../../../shared/enums/app-path.enum";
 import {AuthService} from "../../../authentication/logic/services/auth.service";
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {ChallengeStateEnum} from "../../logic/challenge-state.enum";
+import {PostState} from "../../../posts/logic/enums/post-state.enum";
 
 @Component({
   selector: 'app-all-challenges-page',
@@ -20,27 +21,37 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class AllChallengesPageComponent implements OnInit {
 
+  challengeStateShows = {
+    ALL: 'All',
+    FILLED: 'Already filled',
+    UNFILLED: 'Not yet filled',
+  };
   challengesListObs: Observable<ChallengeDtoModel[]>
   filterOpen: boolean = false;
   myId: string;
   errorMessageObs: Observable<string>;
   isModerator: boolean = false;
   isAdmin: boolean = false;
-  formGeneral: FormGroup;
+  form: FormGroup;
+  currentCarFilter: Map<string, string>;
+  currentStateFilter: Map<string, string>;
+  challengeStates=ChallengeStateEnum;
+  challengeStateList:string[];
 
   constructor(private challengesService: ChallengesService, private menuService: MenuService,
               private profileService: ProfileService, private actions: Actions,
               private router: Router, private authService: AuthService, private formBuilder: FormBuilder) {
-    this.errorMessageObs = this.actions.pipe(ofType(GET_ALL_CHALLENGES_BY_CAR_FAIL), map((action: GetAllChallengesByCarFail) => action.payload));
   }
 
   ngOnInit(): void {
+    this.challengeStateList= Object.keys(this.challengeStates)
     this.menuService.activeRoute.next(ActiveRoute.CHALLENGE)
     this.myId = this.profileService.getMyId();
     this.challengesListObs = this.challengesService.getAllChallenges()
     this.isModerator = this.authService.isModerator()
-    this.formGeneral = this.formBuilder.group({
-      general: new FormControl('')
+    this.form = this.formBuilder.group({
+      general: new FormControl(''),
+      state: new FormControl('')
     })
     this.isAdmin = this.authService.isAdmin();
     window.scrollTo(0, 0)
@@ -58,27 +69,42 @@ export class AllChallengesPageComponent implements OnInit {
     this.filterOpen = !this.filterOpen;
   }
 
-  onChangeGeneral() {
-    if (this.isGeneral()) {
-      this.challengesListObs = this.challengesService.getAllGeneralChallenges()
-    }else{
-      this.clearFilter()
-    }
-  }
-
   applyCarFilter(params: Map<string, string>) {
-    this.challengesListObs = this.challengesService.getAllChallengesByCar(params);
+    this.currentCarFilter=params;
+    this.refreshChallenges()
+  }
+  filledChange() {
+    if(ChallengeStateEnum[this.form.controls.state.value]!=ChallengeStateEnum.ALL){
+      let paramMap = new Map<string, string>();
+      paramMap.set('state', ChallengeStateEnum[this.form.controls.state.value].valueOf());
+      this.currentStateFilter = paramMap;
+    }else {
+      this.currentStateFilter = null;
+    }
+    this.refreshChallenges()
   }
 
   clearFilter() {
-    this.challengesListObs = this.challengesService.getAllChallenges()
+    this.currentCarFilter=null;
+    this.refreshChallenges()
   }
 
   isGeneral() {
-    return this.formGeneral.controls.general.value;
+    return this.form.controls.general.value;
   }
 
   deleteChallenge(id:string){
     this.challengesService.deleteChallenge(id);
+    this.refreshChallenges()
   }
+
+  refreshChallenges(){
+    console.dir(this.currentStateFilter)
+    if (this.isGeneral()) {
+      this.challengesListObs = this.challengesService.getAllGeneralChallenges(this.currentCarFilter, this.currentStateFilter)
+    }else{
+      this.challengesListObs = this.challengesService.getAllChallenges(this.currentCarFilter, this.currentStateFilter)
+    }
+  }
+
 }
