764e7cc23e487848cb871ca0872bbd1a9d53c36a
LAzarrr22 <maciek.janik24@gmail.com>
2020-11-28 18:36:51 +0100

    add still loading message when chat open

--- src/app/modules/messages/pages/messages-conversation/messages-conversation.component.ts
+++ src/app/modules/messages/pages/messages-conversation/messages-conversation.component.ts
@@ -1,21 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {UsersService} from "../../../users/logic/services/users.service";
 import {MenuService} from "../../../menu/logic/services/menu.service";
 import {MessageModel} from "../../logic/dto/model/message.model";
 import {MessagesService} from "../../logic/services/messages.service";
 import {MessageDataModel} from "../../logic/dto/response/message-data.model";
+import {Subscription, timer} from "rxjs";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-messages-conversation',
   templateUrl: './messages-conversation.component.html',
   styleUrls: ['./messages-conversation.component.scss']
 })
-export class MessagesConversationComponent implements OnInit {
+export class MessagesConversationComponent implements OnInit, OnDestroy {
 
   messagesData: MessageDataModel;
   messages: MessageModel[];
-  userId: string
+  userId: string;
+  messageSubs: Subscription;
 
   constructor(private route: ActivatedRoute, private menuService: MenuService, private messageService: MessagesService, private userService: UsersService) {
   }
@@ -24,10 +27,11 @@ export class MessagesConversationComponent implements OnInit {
     this.menuService.activeRoute.next('')
     this.userId = this.route.snapshot.paramMap.get('id');
     this.loadMessages();
-    if (this.messages != undefined) {
-      this.messageService.readMessages(this.userId);
-    }
     window.scrollTo(0, 0)
+  }
+
+  ngOnDestroy(): void {
+    this.messageSubs.unsubscribe();
   }
 
   getReceiverName(): string {
@@ -39,9 +43,13 @@ export class MessagesConversationComponent implements OnInit {
   }
 
   loadMessages() {
-    this.messageService.getAllMessages().subscribe(messagesDataList => this.messagesData = messagesDataList.find(messageData => messageData.id == this.userId));
-    if (this.messagesData != undefined) {
-      this.messages = this.messagesData.messages;
-    }
+    this.messageSubs = timer(0, 10000).pipe(switchMap(() => this.messageService.getAllMessages()))
+      .subscribe(messagesDataList => {
+        this.messagesData = messagesDataList.find(messageData => messageData.id == this.userId);
+        if (this.messagesData != undefined) {
+          this.messages = this.messagesData.messages;
+          this.messageService.readMessages(this.userId);
+        }
+      });
   }
 }
