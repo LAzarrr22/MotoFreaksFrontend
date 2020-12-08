aa2a5d36115b656b7e735eca9d2733fe07045d72
LAzarrr22 <maciek.janik24@gmail.com>
2020-12-08 20:48:52 +0100

    add live reloading chats

--- src/app/modules/messages/pages/my-messages/my-messages.component.ts
+++ src/app/modules/messages/pages/my-messages/my-messages.component.ts
@@ -1,18 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MessagesService} from "../../logic/services/messages.service";
-import {Observable} from "rxjs";
+import {Observable, Subscription, timer} from "rxjs";
 import {MessageDataModel} from "../../logic/dto/response/message-data.model";
 import {UsersService} from "../../../users/logic/services/users.service";
 import {MenuService} from "../../../menu/logic/services/menu.service";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-my-messages',
   templateUrl: './my-messages.component.html',
   styleUrls: ['./my-messages.component.scss']
 })
-export class MyMessagesComponent implements OnInit {
+export class MyMessagesComponent implements OnInit, OnDestroy {
 
   messages: Observable<MessageDataModel[]>;
+  chatsSubs: Subscription;
 
   constructor(private messageService: MessagesService, private userService: UsersService, private menuService: MenuService) {
   }
@@ -20,7 +22,17 @@ export class MyMessagesComponent implements OnInit {
   ngOnInit(): void {
     this.menuService.activeRoute.next('')
     this.messages = this.messageService.getAllChats();
+    this.loadChats();
     window.scrollTo(0, 0)
+  }
+
+  ngOnDestroy(): void {
+    this.chatsSubs.unsubscribe();
+  }
+
+  loadChats() {
+    return  this.chatsSubs = timer(0, 10000)
+      .pipe(switchMap(() => this.messages= this.messageService.getAllChats())).subscribe();
   }
 
 }
